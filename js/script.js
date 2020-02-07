81dd92b0332329953472731983589d24bb7d705b
Treinamento <Treinamento@RD723917.raiadrogasil.com>
2020-02-07 09:38:50 -0300

    Calculadora Funcionando

--- js/script.js
+++ js/script.js
@@ -1,16 +1,41 @@
 let visor = document.querySelector('.visor');
 let btnNumericos = document.querySelectorAll('.numerico');
+let btnOperacoes = document.querySelectorAll('.operador');
+// let btnCe = document.querySelectorAll('#CE');
 
 
-const atualizaVisor = (event) =>{
-   let valortecla =  event.target.textContent;    
-    visor.insertAdjacentText('beforeend', valortecla);
+const operacoes = (expressao) => {
+    return eval(expressao);
 
+}
+
+const limparCalculadora = () => {
+    visor.innerText = "";
+}
+
+
+const atualizaVisor = (event) => {
+    let valorTecla = event.target.textContent;
+
+
+    if (valorTecla == "CE") {
+        limparCalculadora();
+        return
+    } else if (valorTecla == "=") {
+        let resultado = operacoes(visor.innerText);
+        console.log(resultado);
+        
+        visor.innerText = resultado;
+
+        return;
+    }
+    visor.insertAdjacentText('beforeend', valorTecla);
 }
 
 btnNumericos.forEach(btnNumerico => {
     btnNumerico.addEventListener('click', atualizaVisor);
-
 });
 
-
+btnOperacoes.forEach(btnOperacao => {
+    btnOperacao.addEventListener('click', atualizaVisor);
+});
